2deba03ddb29d690edb37985b8d468d0e05b313d
paulo <paulotec96@gmail.com>
2021-05-08 03:26:14 -0300

    Refactoring

--- src/components/ItemLists.jsx
+++ src/components/ItemLists.jsx
@@ -2,9 +2,10 @@ import React from 'react'
 
 const ItemLists = () => {
 
-    const axios = require('axios');
-    const { data } = axios.get('/products/')
-    
+    const axios = require('axios')
+    const response = axios.get('/products/')
+    const { data } = response
+
 
     return (
         <div>
