dd4969c4bd14bc44392e5554761c8a4678eb6d4c
paulo <paulotec96@gmail.com>
2021-05-08 16:00:21 -0300

    Improving Item component

--- src/components/Item.jsx
+++ src/components/Item.jsx
@@ -1,11 +1,55 @@
 import { Card } from '@material-ui/core'
 import React from 'react'
+import { makeStyles } from '@material-ui/core/styles'
+import CardActionArea from '@material-ui/core/CardActionArea'
+import CardActions from '@material-ui/core/CardActions'
+import CardContent from '@material-ui/core/CardContent'
+import CardMedia from '@material-ui/core/CardMedia'
+import Button from '@material-ui/core/Button'
+import Typography from '@material-ui/core/Typography'
+
 
 const Item = ( {item} ) => {
+    const useStyles = makeStyles({
+        root: {
+          maxWidth: 345,
+        },
+        media: {
+          height: 140,
+        },
+      });
+
+    const classes = useStyles();
+
     return (
-        <Card>
-            
-        </Card>
+        <Card className={classes.root}>
+          <CardActionArea>
+            <CardMedia
+              className={classes.media}
+              image={`${item.image}`}
+              title="Product's Picture"
+            />
+            <CardContent>
+              <Typography gutterBottom variant="h5" component="h2">
+                {item.title}
+              </Typography>
+              <Typography gutterBottom variant="h5" component="h2">
+                {item.price}
+              </Typography>
+              <Typography variant="body2" color="textSecondary" component="p">
+                {item.description}
+              </Typography>
+            </CardContent>
+          </CardActionArea>
+          <CardActions>
+            <Button size="small" color="primary">
+              Share
+            </Button>
+            <Button size="small" color="primary">
+              Learn More
+            </Button>
+          </CardActions>
+      </Card>
     )
 }
 
