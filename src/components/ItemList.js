11d25c90587c05b1fe22c5ced93dc687cf17cf01
paulo <paulotec96@gmail.com>
2021-05-08 19:24:24 -0300

    Fixing things

--- src/components/ItemList.js
+++ src/components/ItemList.js
@@ -1,14 +1,23 @@
 import { Grid } from '@material-ui/core'
-import React from 'react'
+import React, { useEffect, useState } from 'react'
 import Item  from './Item'
 
-const ItemList = async () => {
+const ItemList = () => {
 
+    
     const axios = require('axios')
-    const response = await axios.get('/products/')
-    const { data } = response
-
-
+    const [data, setData] = useState([]);
+    useEffect(() => {
+      const fetchData = async () => {
+        const result = await axios(
+          '/products/',
+        );
+   
+        setData(result.data);
+      };
+      fetchData()
+    }, [axios]);
+  
     return (
       <Grid item xs={12}>
         <Grid container justify="center" spacing="3">
